ba1d803a9203ef5b9301ffb093083fe1ebc246bb
luozengcai <luozengcai@yddkt.com>
2019-01-14 16:40:56 +0800

    添加post解析器

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+var bodyParser = require("body-parser")
+
 module.exports = {
   css: {
     loaderOptions: {
@@ -18,6 +20,7 @@ module.exports = {
   configureWebpack: {
     devServer: {
       before(app) {
+        //中间件
         app.use(function(req,res,next){
           if (/^\/api/.test(req.path)) {//只判断请求接口
             if (req.path == '/api/login' || req.headers.token) {
@@ -29,8 +32,12 @@ module.exports = {
             next()
           }
         })
+        //安装post body解析器
+        app.use(bodyParser.urlencoded({extended:true}))
 
         app.get('/api/goods',function(req,res){
+          const { id, foo } = req.query
+          console.log(id,foo)
           res.json({
             code: 0,
             list: [{
@@ -52,8 +59,9 @@ module.exports = {
           })
         })
 
-        app.get('/api/login',function(req,res){
-          const {username , password} = req.query
+        app.post('/api/login',function(req,res){
+          console.log(req.body)
+          const { username, password } = req.body
           if (username == 'admin' && password == 'yddkt123') {
             let token = new Date().getTime() + 7*24*3600*1000
             res.json({
@@ -63,7 +71,7 @@ module.exports = {
           } else {
             res.json({
               code: 1,
-              message: '您登录失败了'
+              message: '您登录失败了啊'
             })
           }
         })
