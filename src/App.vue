1f2aa069c018f0836025fa1a9555a6e6db56c6ac
luozengcai <luozengcai@yddkt.com>
2019-01-16 19:21:08 +0800

    轮播图

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,105 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>|
-      <router-link to="/login" v-if="!isLogin">Login</router-link>
-      <a v-else @click="logout">Logout</a>
-    </div>
-    <router-view/>
+    <transition name="route-move">
+      <router-view class="child-view"/>
+    </transition> 
+    <cube-tab-bar
+      showSlider
+      v-model="selectedLabelDefault"
+      :data="tabs"
+      @change="changeHandler"
+    ></cube-tab-bar>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
+import { mapGetters } from "vuex";
 export default {
+  data() {
+    return {
+      selectedLabelDefault: '/',
+      tabs: [{
+        value: '/',
+        label: 'Home',
+        icon: 'cubeic-home'
+      }, {
+        value: '/cart',
+        label: 'Cart',
+        icon: 'cubeic-mall'
+      }, {
+        value: '/login',
+        label: 'Me',
+        icon: 'cubeic-person'
+      }]
+    }
+  },
+  watch: {
+    $route(router){
+      this.selectedLabelDefault = router.path
+    }
+  },
+  created(){
+    this.selectedLabelDefault = this.$route.path
+  },
   methods: {
     logout() {
-      this.$http.get('/api/logout')
+      this.$http.get("/api/logout")
+    },
+    changeHandler(val){
+      this.$router.push(val)
     }
   },
   computed: {
-    ...mapGetters(['isLogin'])
-  },
-}
+    ...mapGetters(["isLogin"])
+  }
+};
 </script>
 
-
-<style>
+<style scoped>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
-#nav {
-  padding: 30px;
-}
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.cube-tab-bar{
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: #edf0f4;
 }
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.cube-tab-bar-slider {
+  top: 0;
+}
+/* 页面滑动动画 */
+/* 入场前 */
+.route-move-enter {
+  transform: translate3d(-100%, 0, 0);
+}
+/* 出场后 */
+.route-move-leave-to {
+  transform: translate3d(100%, 0, 0);
+}
+.route-move-enter-active,
+.route-move-leave-active {
+  transition: transform 0.3s;
+}
+.child-view {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  padding-bottom: 36px;
+}
+span.badge {
+  display: inline-block;
+  background: #de3529;
+  color: white;
+  width: 1rem;
+  height: 1rem;
+  border-radius: 50%;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="home">
+    <cube-slide :data="slide" :interval="3000">
+      <cube-slide-item
+        v-for="item in slide" :key="item.id">
+        <router-link to="/cart">
+          <img :src="item.img">
+        </router-link>
+      </cube-slide-item>
+    </cube-slide>
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
@@ -7,20 +15,36 @@
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'home',
+  name: "home",
   components: {
     HelloWorld
   },
-  created () {
-    this.$http.get('/api/goods',{
-      params: {
-        id: 1,
-        foo: 'bar'
-      }
-    })
+  data() {
+    return {
+      slide: [],
+      goods: []
+    };
+  },
+  async created() {
+    const res = await this.$http.get("/api/goods", {
+      params: { id: 1, foo: "bar" }
+    });
+    console.log(res);
+    const { slider, keys, data } = res.data;
+    this.slide = slider;
+    this.goods = data;
   }
-}
+};
 </script>
+<style lang="stylus">
+.cube-slide {
+  height: auto;
+}
+.cube-slide-item > a > img {
+  width: 100%;
+}
+</style>
+
